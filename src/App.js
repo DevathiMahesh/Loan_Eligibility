a5e509360c7a6eddcf903118387b106fa744ff70
DevathiMahesh <d.mahesh995@gmail.com>
2020-12-03 18:16:49 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router,Route} from 'react-router-dom';
 function App() {
   return (
    <Router>
-     <Route exact path="/home" component={Home}/>
+     <Route exact path="/" component={Home}/>
    </Router>
   );
 }
